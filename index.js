695e55a6e653e98028a507cad12cb0f09d56aaf8
Matthew Riegg <matthew.d.riegg@gmail.com>
2021-05-18 14:58:42 -0700

    commit tree f03dba4086e3dda462123e1131f75f7a0c28aee6

--- index.js
+++ index.js
@@ -11,6 +11,33 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (titleArray) => {titleArray.map(x => x.toTitleCase())
 }
+
+
+console.log(titleCased(tutorials))
+
+
+// `map()` takes 2 arguments — a callback and the optional context. The callback is
+// called for each value in the original array and the modified value is added to a
+// new array. Its return value is a new array that is the same length as the
+// original array. Using `map()` saves time while making the code simpler and more
+// expressive.
+
+
+
+// const array1 = [1, 4, 9, 16];
+
+// // pass a function to map
+// const map1 = array1.map(x => x * 2);
+
+// console.log(map1);
+// expected output: Array [2, 8, 18, 32]
+
+
+// split and join
+
+// own titleCase function
+// used map
+
+// then used map to call function i made up 
